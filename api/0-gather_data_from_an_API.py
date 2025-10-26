b661e7d721107931113ac24611d17ed3fcfaf5dc
dumethode <m.duhujubum@alustudent.com>
2025-10-26 02:44:14 +0200

    Update

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,27 +1,34 @@
 #!/usr/bin/python3
 """
-Using a REST API, and a given emp_ID, return info about their TODO list.
+Script that fetches TODO list progress for a given employee ID from an API
 """
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    """ main section """
-    BASE_URL = 'https://jsonplaceholder.typicode.com'
-    employee = requests.get(
-        BASE_URL + f'/users/{sys.argv[1]}/').json()
-    EMPLOYEE_NAME = employee.get("name")
-    employee_todos = requests.get(
-        BASE_URL + f'/users/{sys.argv[1]}/todos').json()
-    serialized_todos = {}
+    employee_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com"
 
-    for todo in employee_todos:
-        serialized_todos.update({todo.get("title"): todo.get("completed")})
+    user_url = "{}/users/{}".format(base_url, employee_id)
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
+    employee_name = user_data.get("name")
 
-    COMPLETED_LEN = len([k for k, v in serialized_todos.items() if v is True])
+    todos_url = "{}/todos?userId={}".format(base_url, employee_id)
+    todos_response = requests.get(todos_url)
+    todos = todos_response.json()
+
+    completed_tasks = []
+    for task in todos:
+        if task.get("completed") is True:
+            completed_tasks.append(task)
+
+    total_tasks = len(todos)
+    num_completed = len(completed_tasks)
+
     print("Employee {} is done with tasks({}/{}):".format(
-        EMPLOYEE_NAME, COMPLETED_LEN, len(serialized_todos)))
-    for key, val in serialized_todos.items():
-        if val is True:
-            print("\t {}".format(key))
+        employee_name, num_completed, total_tasks))
+
+    for task in completed_tasks:
+        print("\t {}".format(task.get('title')))
